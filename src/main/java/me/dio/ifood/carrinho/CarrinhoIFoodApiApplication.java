f823c8a018c7b2f09208a942b9aef59123badbb3
Jean Wylmer <jean_wylmer@hotmail.com>
2022-09-30 11:58:11 -0300

    feat: init project

---
+++ src/main/java/me/dio/ifood/carrinho/CarrinhoIFoodApiApplication.java
@@ -0,0 +1,13 @@
+package me.dio.ifood.carrinho;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarrinhoIFoodApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarrinhoIFoodApiApplication.class, args);
+	}
+
+}
